b73553f24c94b61afbf0d2a0797b138712ba4e03
jakeg <jake.goulding@live.com>
2026-03-01 13:51:25 -0500

    Allow negative cost attributes via bAllowNegativeCost flag on FP_GameplayAbility

--- Source/FP_5_7/Private/AbilitySystem/Abilities/FP_GameplayAbility.cpp
+++ Source/FP_5_7/Private/AbilitySystem/Abilities/FP_GameplayAbility.cpp
@@ -1,6 +1,15 @@
 ﻿#include "AbilitySystem/Abilities/FP_GameplayAbility.h"
 #include "AbilitySystemComponent.h"
 
+bool UFP_GameplayAbility::CheckCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, FGameplayTagContainer* OptionalRelevantTags) const
+{
+	if (bAllowNegativeCost)
+	{
+		return true;
+	}
+	return Super::CheckCost(Handle, ActorInfo, OptionalRelevantTags);
+}
+
 UAbilitySystemComponent* UFP_GameplayAbility::GetSourceASC() const
 {
 	return GetAbilitySystemComponentFromActorInfo();
--- Source/FP_5_7/Public/AbilitySystem/Abilities/FP_GameplayAbility.h
+++ Source/FP_5_7/Public/AbilitySystem/Abilities/FP_GameplayAbility.h
@@ -16,7 +16,14 @@ class FP_5_7_API UFP_GameplayAbility : public UGameplayAbility
 public:
 	UPROPERTY(EditDefaultsOnly, Category="Input", meta = (Categories = "InputTag"))
 	FGameplayTag StartupInputTag;
-		
+
+	// If true, bypasses the GAS cost check that blocks activation when the cost would take an attribute below 0.
+	// Use this for abilities whose cost attribute (e.g. Heat) is designed to go negative.
+	UPROPERTY(EditDefaultsOnly, Category="Cost")
+	bool bAllowNegativeCost = false;
+
+	virtual bool CheckCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+
 protected:
 	UFUNCTION(BlueprintPure, Category="FP|GAS")
 	UAbilitySystemComponent* GetSourceASC() const;
